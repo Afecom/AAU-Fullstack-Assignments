f00e132551542689bea0d24e1213d5f497e3a9f4
Nuru Kamil <88211665+Afecom@users.noreply.github.com>
2025-08-01 11:10:08 +0300

    Delete src directory

--- src/genres/dto/create-genre.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-
-export class CreateGenreDto {
-  @ApiProperty({ example: 'Fiction', description: 'Genre name' })
-  @IsString()
-  name: string;
-} 
