da587e60e0efa930d6755312bf5eea13a8e9ce38
Nuru Kamil <knuru472@gmail.com>
2025-08-20 19:30:58 +0300

    added product images to model and db

---
+++ backend-gig/db-arcade/assignment-express-postgres-sequelize-express_E-commerce/db/models/products.model.js
@@ -0,0 +1,63 @@
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+    class Product extends Model {
+        static associate(models) {
+            Product.belongsTo(models.Category, {
+                foreignKey: 'category_id',
+                as: 'categories'
+            }),
+            Product.hasMany(models.Product_image, {
+                foreignKey: 'product_id',
+                as: 'images'
+            }),
+            Product.hasMany(models.Order_item, {
+                foreignKey: 'product_id',
+                as: 'order_items'
+            }),
+            Product.hasMany(models.Cart, {
+                foreignKey: 'product_id',
+                as: 'cart_items'
+            })
+        }
+    }
+
+    Product.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Category',
+                key: 'id'
+            }
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },{
+        sequelize,
+        timestamps: true,
+        modelName: 'Product',
+        tableName: 'Products'
+    })
+
+    return Product
+}
